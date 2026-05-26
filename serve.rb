b0dacb178f84fd13664f755a5f540fe74925168c
VivaSante <vivasante@MacBook-Pro-de-Benjamin.local>
2026-05-26 22:44:47 +0200

    Intègre les vrais logos + favicon

---
+++ serve.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require 'webrick'
+dir = File.expand_path('..', __FILE__)
+server = WEBrick::HTTPServer.new(
+  Port: 3456,
+  DocumentRoot: dir,
+  Logger: WEBrick::Log.new('/dev/null'),
+  AccessLog: []
+)
+trap('INT') { server.shutdown }
+server.start
